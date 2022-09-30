d6edd78e023ad589f827319b0641dcb0d1af47c9
tatekitzhak <tatekosi@gmail.com>
2022-09-30 22:57:52 +0300

    Added and configer a redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducers from '@/store/reducer/index';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import sessionStorage from 'redux-persist/lib/storage/session'
+import { PersistGate } from 'redux-persist/integration/react'
+//import rootReducers from '@/store/reducer/index';
+import { userReducers } from "@/store/reducer/reducer";
 
 /// store holds the state of the application
 /* 
@@ -10,22 +15,46 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)));
  */
+const userPersistConfig = {
+    key: 'user',
+    storage: sessionStorage,
+}
 
-const store = createStore(rootReducers,
+const rootPersistConfig = {
+    key: 'root',
+    storage: storage,
+    //storage:storageSession,
+    // blacklist: ['apiProductSlice'],
+    whitelist: ['users'],
+    /* debug: true,
+   whitelist: ['activity', 'user'] */
+}
+const rootReducers = combineReducers({
+    user: persistReducer(userPersistConfig, userReducers),
+    // notes: notesReducer
+});
+
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducers)
+
+
+const store = createStore(persistedReducer,
     compose(
         applyMiddleware(
             thunk,
         )
     )
 );
-
+const persistor = persistStore(store);
 
 function ReduxWrapper(props) {
     return (
-      <Provider store={store}>
-          {props.children}
-      </Provider>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {props.children}
+            </PersistGate>
+        </Provider>
     )
-  }
-  
-  export default ReduxWrapper;
+}
+
+export default ReduxWrapper;
