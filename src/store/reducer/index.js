c5beb90288b9c56b24fe9e69981cfc700f332591
Ran Itsak <54587260+tatekitzhak@users.noreply.github.com>
2022-10-01 09:03:15 +0300

    Delete src directory

--- src/store/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { userReducers } from "./reducer";
-const rootReducers = combineReducers({
-    user: userReducers
-});
-export default rootReducers;
